1e993bc5ccd17dd8991ee033f8e61a754ba2c3c0
Sina_Shokri <sinashokri02@gmail.com>
2025-01-11 15:26:59 +0330

    edit 2

--- Amir.cpp
+++ Amir.cpp
@@ -2,4 +2,5 @@
 using namespace std;
 int main(){
     cout<<"hi Amir :D";
+    cout<<"hi Amir 2";
 }
--- Sina.cpp
+++ Sina.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 int main(){
     cout<<"hi sina";
-    cout<<"tagir1";
+    cout<<"tagir2";
 }
